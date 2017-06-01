1b702d54183256fa96edf3729b0ceda66d62657e
manos <manos@sobol>
2017-06-01 19:32:34 +0300

    testing gh-pages deployment

--- config/deploy.js
+++ config/deploy.js
@@ -1,28 +1,43 @@
-  /* jshint node: true */
+/* jshint node: true */
 
 module.exports = function(deployTarget) {
-  deployTarget || (deployTarget = 'dev');
-  ENV.build = {
-  };
   var ENV = {
-    // always build the production environment
-    build: {
-        environment: 'production' // the default
-    }
+      // always build the production environment
+      build: {
+          environment: 'production' // the default
+      },
+      // include other plugin configuration that applies to all deploy targets here
+
+      git: {
+          // The repo that we will deploy to. It defaults to the value of your containing repo's origin remote
+          // repo: 'git@github.com:Noteblox/client-ember.git',
+
+          // The branch that we will deploy to. It must already exist. Defaults to "gh-pages"
+          // branch: 'gh-pages',
+
+          // Path where we will create/update a working tree to manipulate the deployment branch.
+          // Defaults to ../deploy-${project.name()}, relative to your project.
+          // worktreePath: '../deploy-${project.name()}',
+
+          // Message to use when committing the deployment, where %@ is replaced with the current git revision.
+          commitMessage: 'Deployed %@'
+      }
   };
 
-
-
-  if (deployTarget === 'embed') {
-    ENV.build.environment = 'embed';
-    // configure other plugins for staging deploy target here
-
-
-    console.log("embedded: "  );
-    ENV.APP.rootElement = '#restdude-embedded';
-    ENV.locationType = 'none';
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
   }
 
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
