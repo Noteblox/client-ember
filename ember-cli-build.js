13e93b048a7b298472b1b0dc462884892fbbc822
manos <manos@sobol>
2017-06-02 00:34:56 +0300

    testing gh-pages deployment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+
+    fingerprint: {
+        exclude: ['fonts'],
+        prepend: 'https://noteblox.github.io/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
